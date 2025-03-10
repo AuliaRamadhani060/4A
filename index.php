807e279d130e4a3043c8f87807fc9ced85945390
Aulia Ramadhani060 <E020323060@mahasiswa.poliban.ac.id>
2025-03-10 13:05:42 +0800

    membuat file prodi.php 10/3/25

--- index.php
+++ index.php
@@ -8,5 +8,27 @@
 
 <body>
     <H1>DATA MAHASISWA</H1>
+    <br>
+    <table border="1" cellspacing="0" cellpadding="5">
+        <thead>
+            <tr>
+                <td>No</td>
+                <td>NIM</td>
+                <td>Nama</td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>E020323060</td>
+                <td>Aulia Ramadhani</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>E020323090</td>
+                <td>Ucup</td>
+            </tr>
+        </tbody>
+    </table>
 </body>
 </html>
