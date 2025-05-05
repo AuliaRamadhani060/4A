b031361e32ed06a8391e54918a6624c248947892
Aulia Ramadhani060 <E020323060@mahasiswa.poliban.ac.id>
2025-05-05 16:30:49 +0800

    membuat template

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <?php
 session_start();
 if (!isset($_SESSION['login'])) {
@@ -10,59 +11,85 @@ $query = "SELECT mahasiswa.*, prodi.Nama AS ProdiNama
           FROM mahasiswa 
           JOIN prodi ON mahasiswa.Id = prodi.Id";
 $data = ambildata($query);
+
+include "template/header.php";
+include "template/sidebar.php";
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SIMPADU POLIBAN </title>
-</head>
+<main class="app-main">
+        <!--begin::App Content Header-->
+        <div class="app-content-header">
+          <!--begin::Container-->
+          <div class="container-fluid">
+            <!--begin::Row-->
+            <div class="row">
+              <div class="col-sm-6"><h3 class="mb-0">Dashboard</h3></div>
+              <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-end">
+                  <li class="breadcrumb-item"><a href="#">Home</a></li>
+                  <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+                </ol>
+              </div>
+            </div>
+            <!--end::Row-->
+          </div>
+          <!--end::Container-->
+        </div>
+        <!--end::App Content Header-->
+        <!--begin::App Content-->
+        <div class="app-content">
+          <!--begin::Container-->
+          <div class="container-fluid">
+            <!--begin::Row-->
+            <div class="row">
+              <div class="col-md-12">
+                <div class="card mb-4">
+                  <div class="card-header"><h3 class="card-title">DATA MAHASISWA</h3></div>
+                  <!-- /.card-header -->
+                  <div class="card-body">
+                    <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>NIM</th>
+                            <th>Nama</th>
+                            <th>Tanggal lahir</th>
+                            <th>Prodi</th>
+                            <th>Telpon</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php 
+                            $i=1;
+                            foreach($data as $d) : ?>
+                                <tr>
+                                    <td><?php echo $i++; ?></td>
+                                    <td><?= $d["NIM"] ?></td>
+                                    <td><?= $d["Nama"] ?></td>
+                                    <td><?= $d["TanggalLahir"] ?></td>
+                                    <td><?= $d["ProdiNama"] ?></td>
+                                    <td><?= $d["Telp"] ?></td>
+                                    <td>
+                                        <a class="btn btn-warning" href="editmahasiswa.php?NIM=<?= $d["NIM"] ?>">Edit</a> 
+                                        <a class= "btn btn-danger" href="hapusmahasiswa.php?NIM=<?= $d["NIM"]  ?>" onclick="return confirm('Apakah Anda yakin ingin menghapus data ini?')">Hapus</a> 
+                                    </td>
+                                </tr>
 
-<body>
-    <H1>DATA MAHASISWA</H1>
-    <br>
-    <a href="TambahMahasiswa.php">Tambah</a>
-    <table border="1" cellspacing="0" cellpadding="5">
-        <thead>
-            <tr>
-                <td>No</td>
-                <td>NIM</td>
-                <td>Nama</td>
-                <td>Tanggal lahir</td>
-                <td>Prodi</td>
-                <td>Telpon</td>
-                <td>Email</td>
-                <td>Action</td>
-            </tr>
-        </thead>
-        <tbody>
+                        <?php endforeach; ?>
 
-        <?php 
-        $i=1;
-        foreach($data as $d) : ?>
-
-
-            <tr>
-                <td><?php echo $i++; ?></td>
-                <td><?= $d["NIM"] ?></td>
-                <td><?= $d["Nama"] ?></td>
-                <td><?= $d["TanggalLahir"] ?></td>
-                <td><?= $d["ProdiNama"] ?></td>
-                <td><?= $d["Telp"] ?></td>
-                <td><?= $d["Email"] ?></td>
-                
-                <td>
-                    <a href="editmahasiswa.php?NIM=<?= $d["NIM"] ?>">Edit</a> |
-                    <a href="hapusmahasiswa.php?NIM=<?= $d["NIM"]  ?>" onclick="return confirm('Apakah Anda yakin ingin menghapus data ini?')">Hapus</a> 
-                </td>
-            </tr>
-
-        <?php endforeach; ?>
-
-        </tbody>
-    </table>
-    <a href="logout.php">Keluar</a>
-</body>
-</html>
+                    </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- /.row (main row) -->
+          </div>
+          <!--end::Container--> 
+        </div>
+        <!--end::App Content-->
+ </main>
+    <?php
+    include "template/footer.php";
+    ?>
